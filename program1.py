ddaa207b7e687c280c3fbc14c85c2b97e6d07007
Himanshu4782652 <127956737+Himanshu4782652@users.noreply.github.com>
2024-04-26 13:26:06 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,15 +1,13 @@
-def smallest_missing_positive_integer(num, List, input):
+def smallest_missing_positive_integer(num, List):
     List[] = int[1,2,5,7,9]
     num = input()
     input = List[]
     if(input < List[]):
         print("smallest positive integer")
         return num
-    elif:
+    else:
         print("error")
 main(smallest_missing_positive_integer)
-
-
 
 
 
